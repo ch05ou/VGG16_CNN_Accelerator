d518d1232a2a4f906dbae9e1cca36f3b3a246582
Eric <107875654+ch05ou@users.noreply.github.com>
2025-03-15 22:04:33 +0800

    Update AT.v

--- 01_RTL/AT.v
+++ 01_RTL/AT.v
@@ -8,25 +8,25 @@ module AT(
     reg signed[27:0]tmp027,tmp8_3;
 
     always @(*) begin
-        tmp01 <= pe_out_0 + pe_out_1;
-        tmp23 <= pe_out_2 + pe_out_3;
-        tmp45 <= pe_out_4 + pe_out_5;
-        tmp67 <= pe_out_6 + pe_out_7;
-        tmp8 <= pe_out_8;
+        tmp01 = pe_out_0 + pe_out_1;
+        tmp23 = pe_out_2 + pe_out_3;
+        tmp45 = pe_out_4 + pe_out_5;
+        tmp67 = pe_out_6 + pe_out_7;
+        tmp8 = pe_out_8;
     end
 
     always @(*)begin
-        tmp0123 <= tmp01 + tmp23;
-        tmp4567 <= tmp45 + tmp67;
-        tmp8_2 <= tmp8;
+        tmp0123 = tmp01 + tmp23;
+        tmp4567 = tmp45 + tmp67;
+        tmp8_2 = tmp8;
     end
 
     always @(*) begin
-        tmp027 <= tmp0123 + tmp4567;
-        tmp8_3 <= tmp8_2;
+        tmp027 = tmp0123 + tmp4567;
+        tmp8_3 = tmp8_2;
     end
     
     always @(*) begin
-        sum <= tmp027 + tmp8_3;
+        sum = tmp027 + tmp8_3;
     end
 endmodule
